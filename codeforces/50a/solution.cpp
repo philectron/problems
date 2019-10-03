fd03d51d58d26ed08e8f3ead4c6559488b772090
Phi Luu <philectron@users.noreply.github.com>
2019-10-02 21:15:12 -0700

    Complete Codeforces 50A - Domino Pilling

---
+++ codeforces/50a/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m, n;
+
+    cin >> m >> n;
+
+    if (m % 2 == 0 || n % 2 == 0)
+        cout << m * n / 2 << endl;
+    else
+        cout << (m * n - 1) / 2 << endl;
+
+    return 0;
+}
