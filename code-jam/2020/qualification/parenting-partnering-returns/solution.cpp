b51b35202b9f945e3288069379ae3b9ce6cb6a51
Phi Luu <philectron@users.noreply.github.com>
2020-04-03 23:58:53 -0700

    Stress test

--- code-jam/2020/qualification/parenting-partnering-returns/solution.cpp
+++ code-jam/2020/qualification/parenting-partnering-returns/solution.cpp
@@ -20,7 +20,18 @@ bool Overlap(const vector<Activity>& act, const Activity& a) {
     return false;
 }
 
-void Solve(int n, const vector<Activity>& a) {
+bool Test(const vector<Activity>& act) {
+    for (int i = 0, n = act.size(); i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j != i && act[i].OverlapsWith(act[j])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+string Solve(int n, const vector<Activity>& a) {
     string result = "C";
     vector<Activity> c, j;
 
@@ -29,8 +40,14 @@ void Solve(int n, const vector<Activity>& a) {
     for (int i = 1; i < n; i++) {
         bool overlappedC = Overlap(c, a[i]), overlappedJ = Overlap(j, a[i]);
         if (overlappedC && overlappedJ) {
-            cout << "IMPOSSIBLE" << endl;
-            return;
+            result = "IMPOSSIBLE";
+
+            // <DEBUG>
+            c.push_back(a[i]);
+            j.push_back(a[i]);
+            // </DEBUG>
+
+            break;
         } else if (overlappedC) {
             result += 'J';
             j.push_back(a[i]);
@@ -40,9 +57,30 @@ void Solve(int n, const vector<Activity>& a) {
         }
     }
 
-    cout << result << endl;
+    // <DEBUG>
+    system("clear");
+    string impossible = "IMPOSSIBLE";
+    if (result.compare(impossible) == 0) {
+        if (Test(c) && Test(j)) {
+            result = "BUG";
+            cout << "                   BUGGED!" << endl;
+        }
+    } else {
+        if (c.size() + j.size() != n || !Test(c) || !Test(j)) {
+            result = "BUG";
+            cout << "                   BUGGED!" << endl;
+        }
+    }
+    cout << n << endl;
+    for (const Activity& act : a) {
+        cout << act.start << " " << act.end << endl;
+    }
+    // </DEBUG>
+
+    return result;
 }
 
+/*
 int main() {
     int t;
     cin >> t;
@@ -55,8 +93,29 @@ int main() {
             cin >> a[i].start >> a[i].end;
         }
 
-        cout << "Case #" << x << ": ";
-        Solve(n, a);
+        cout << "Case #" << x << ": " << Solve(n, a) << endl;
+    }
+
+    return 0;
+}
+*/
+
+// stress test
+int main() {
+    srand(time(nullptr));
+
+    string result;
+    string found = "BUG";
+
+    while (result.compare(found) != 0) {
+        int n = rand() % (10 - 2 + 1) + 2;
+        vector<Activity> a(n);
+        for (int i = 0; i < n; i++) {
+            a[i].start = rand() % 1440;
+            a[i].end = rand() % (1440 - a[i].start + 1 + 1) + a[i].start + 1;
+        }
+
+        result = Solve(n, a);
     }
 
     return 0;
