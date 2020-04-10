793596ce9c83c507fe4849179685cc19768db02f
Phi Luu <philectron@users.noreply.github.com>
2020-04-10 16:56:56 -0700

    Init template for Google Code Jam 2020 Round 1A

---
+++ code-jam/2020/1a/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Solve() {}
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int x = 1; x <= t; x++) {
+        cout << "Case #" << x << ": ";
+        Solve();
+    }
+
+    return 0;
+}
