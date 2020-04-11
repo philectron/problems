c046ffd234d7479e476b3254e8f84dc06678b0ab
Phi Luu <philectron@users.noreply.github.com>
2020-04-10 19:48:47 -0700

    Done

---
+++ code-jam/2020/1a/b-pascal-walk/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Solve(int n) {
+
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int x = 1; x <= t; x++) {
+        int n;
+        cin >> n;
+
+        cout << "Case #" << x << ":" << endl;
+        Solve(n);
+    }
+
+    return 0;
+}
